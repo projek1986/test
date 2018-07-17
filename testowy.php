0a4463dfdb0635345fe54439bcbc94f34059324b
projek <rojek.przemek@gmail.com>
2018-07-17 14:52:20 +0200

    soap CIDG

--- testowy.php
+++ testowy.php
@@ -1,3 +1,16 @@
-<?php
+<soapenv:Envelope xmlns:soapenv="http://shemas.xmlsoap.org/soap/envelope/"
+                  xnlns:tem="htpp://tempuri.org"
+                  xmlns:arr="http://shemas.microsoft.com/2003/10/Serialization/Arrays">
+
+    <soapenv:Header/>
+    <soapenv:Body>
+       <tem:GetMigrationDataExtendedAdressInfo>
+           <tem:AuthToken></tem:AuthToken>
+           <tem:NIP>
+               <arr:string>5840778201</arr:string>
+           </tem:NIP>
+       </tem:GetMigrationDataExtendedAdressInfo>
+      </soapenv:Body>
+</soapenv:Envelope>
 
 
